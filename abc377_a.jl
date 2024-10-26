13803e77cd00a43267ff33d425e7a7c25c09c712
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-26 18:30:26 +0900

    :up:

---
+++ abc377_a.jl
@@ -0,0 +1,10 @@
+function main()
+    bomora
+end
+
+parseint() = readline() |> x -> parse(Int, x)
+parsestring() = readline()
+parseints() = readline() |> split |> x -> parse.(Int, x)
+parsestrings() = readline() |> split
+
+main()
