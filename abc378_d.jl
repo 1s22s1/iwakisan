97bdc193498f475a49e89ab14f41c8540e67dfb9
1s22s1 <1s22s1@users.noreply.github.com>
2024-11-02 22:01:03 +0900

    :up:

--- abc378_d.jl
+++ abc378_d.jl
@@ -7,13 +7,11 @@ function main()
     for y ∈ 1:h, x ∈ 1:w
         if s[y][x] == "."
             for (a, b) ∈ [[1, 0], [-1, 0], [0, -1], [0, 1]]
-                adjacent_x = x + a
-                adjacent_y = y + b
+                adjacent_x = x+a
+                adjacent_y = y+b
 
-                if 1 ≤ adjacent_x ≤ w &&
-                   1 ≤ adjacent_y ≤ h &&
-                   s[adjacent_y][adjacent_x] == "."
-                    push!(g[(y-1)*w+x], (adjacent_y - 1) * w + adjacent_x)
+                if 1 ≤ adjacent_x ≤ w && 1 ≤ adjacent_y ≤ h && s[adjacent_y][adjacent_x] == "."
+                    push!(g[(y-1)*w + x], (adjacent_y-1)*w + adjacent_x)
                 end
             end
         end
@@ -23,27 +21,37 @@ function main()
     global result = 0
 
     for i ∈ 1:h*w
-        global seen = fill(false, length(g))
-        dfs(i, k)
+        seen = bfs(g, i)
+        @show seen
+        result += length(filter(x -> x == k, seen))
     end
 
     println(result)
 end
 
-function dfs(vertex, k)
-    seen[vertex] = true
 
-    for next_vertex ∈ g[vertex]
-        if seen[next_vertex]
-            continue
+function bfs(graph::Vector{}, start::Int)
+    nexts = [start]
+
+    seen = fill(-1, length(graph))
+    pop_count = 0
+    seen[start] = pop_count
+
+    while !isempty(nexts)
+        target_vertex = popfirst!(nexts)
+        pop_count += 1
+
+        for vertex ∈ graph[target_vertex]
+            if seen[vertex] ≠ -1
+                continue
+            end
+
+            push!(nexts, vertex)
+            seen[vertex] = pop_count
         end
-
-        dfs(next_vertex, k - 1)
     end
 
-    if k == 0
-        global result += 1
-    end
+    return seen::Vector{}
 end
 
 parseint() = readline() |> x -> parse(Int, x)
